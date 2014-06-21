8e0c45061660acdd11d93c000f09674fa7adce52
Chris DeLuca <bronzehedwick@gmail.com>
2014-06-21 09:53:26 -0400

    Update time on interval.

--- js/main.js
+++ js/main.js
@@ -226,6 +226,9 @@ function mce_success_cb(resp){
   // Date
   var cymDate = document.getElementsByTagName('time');
   cymDate[0].innerHTML = moment().format('hh:m A');
+  setInterval(function() {
+    cymDate[0].innerHTML = moment().format('hh:m A');
+  }, 1000);
 
   // Google Calendar
   $('#calendar').gCalFlow({
